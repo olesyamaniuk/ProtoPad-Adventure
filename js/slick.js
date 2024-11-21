99111c283f1a41a677e530e492a98eb8dbdf3f87
olesyamaniuk <maniukolesya222@gmail.com>
2024-11-21 17:16:28 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -16,7 +16,23 @@ $(function () {
   });
 });
 
-
+$(function () {
+  $('.blog__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: false,
+    cssEase: 'linear',
+    prevArrow: '.blog__list-left',
+    nextArrow: '.blog__list-right',
+    
+  });
+});
 
 
 
